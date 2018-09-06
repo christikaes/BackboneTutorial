f067cf986d945e1e23ba7a9e73d05cdb074058c5
Christina Kayastha <ckayastha@vistaprint.com>
2018-09-06 12:46:35 -0400

    Part4-Events

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var AppView = Backbone.View.extend({
     // el - stands for element. Every view has an element associated with HTML content, will be rendered. 
     el: "#root",
 
+    // Events
+    events: {
+        'click #add': 'onAdd'
+    },
+    
+    onAdd: function() {
+        this.items.add(new Item({content: "I am a new Item!"}));
+        this.render();
+    },
+
     // It's the first function called when this view is instantiated.
     initialize: function() {
         var item1 = new Item({ content: "I am item 1!"});
@@ -16,18 +26,20 @@ var AppView = Backbone.View.extend({
         var item3 = new Item({ content: "I am item 3!"});
 
         var items = new Items([item1, item2, item3]);
+        this.items = items;
 
         this.render(items);
     },
 
     // $el - it's a cached jQuery object (el), in which you can use jQuery functions to push content.
-    render: function(items) {
-        var list = "<ol>";
-        items.forEach((item) => {
+    render: function() {
+        var list = "<ol id='items'>";
+        this.items.forEach((item) => {
             list += `<li> ${item.get("content")} </li>`;
         })
         list += "</ol>";
-        this.$el.html(list);
+        var button = "<button id='add'>Add Item!</button>"
+        this.$el.html(list + button);
     }
  });
  var appView = new AppView();
