0b504b64488a4b3d85725166e6a0345849c55188
Christina Kayastha <ckayastha@vistaprint.com>
2018-09-06 12:35:53 -0400

    Part2-Model

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+// Item Model
+var Item = Backbone.Model.extend({});
+
+// App View
 var AppView = Backbone.View.extend({
     // el - stands for element. Every view has an element associated with HTML content, will be rendered. 
     el: "#root",
 
     // It's the first function called when this view is instantiated.
     initialize: function() {
-       this.render();
+        var itemModel = new Item({ content: "I am an item!"});
+
+        this.render(itemModel);
     },
 
     // $el - it's a cached jQuery object (el), in which you can use jQuery functions to push content.
-    render: function() {
-       this.$el.html("Hello World!");
+    render: function(itemModel) {
+       this.$el.html(itemModel.get("content"));
     }
  });
  var appView = new AppView();
